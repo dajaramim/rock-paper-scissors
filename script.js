3489e66f8590711e4667cceff6dcdbb439f8f7fc
Daniel Jaramillo Moya <da.jaramim@gmail.com>
2023-12-01 14:02:22 -0300

    feat: score logic and ui added

--- script.js
+++ script.js
@@ -1,39 +1,63 @@
 const gameOption = ["rock", "paper", "scissors"]
+const play = document.querySelector('#play');
+const reset = document.querySelector('#reset');
+const scorePlayerHTML = document.querySelector('#score-player');
+const scoreComputerHTML = document.querySelector('#score-cpu');
 const choices = document.querySelectorAll('#choice');
-const game = document.querySelector('#game');
+const gameResult = document.querySelector('#game-result');
 let result;
 let playerChoice;
 let computerChoice;
+let playerScore = 0;
+let computerScore = 0;
 
 eventListeners()
 function eventListeners() {
     choices.forEach(choice => {
         choice.addEventListener('click', getPlayerChoice)
     });
+    play.addEventListener('click', playAgain)
+    reset.addEventListener('click', resetScore)
 }
 
 // Functions
 
+function playAgain() {
+    clearHTML()
+    clearChoice()
+}
+function resetScore() {
+    playAgain()
+    playerScore = 0;
+    computerScore = 0;
+    scorePlayerHTML.textContent = 0;
+    scoreComputerHTML.textContent = 0;
+}
+
 function getPlayerChoice(e) {
     clearChoice()
+    const selectedChoice = !e.target.classList.contains("choice") ? e.target.parentNode : e.target;
+    if (playerScore + computerScore >= 5) return
     computerChoice = getComputerChoice()
-    choices.forEach(choice => {
-        if (choice.getAttribute('data-id') === computerChoice) {
-            choice.classList.add('computer','choice-selected');
-        }
-    });
+
     if (!e.target.classList.contains("choice")) {
         playerChoice = e.target.parentNode.getAttribute('data-id')
     } else {
         playerChoice = e.target.getAttribute('data-id')
     }
-    const selectedChoice = !e.target.classList.contains("choice") ? e.target.parentNode : e.target;
-    selectedChoice.classList.add('choice-selected');
+    if (playerChoice === computerChoice) {
+        selectedChoice.classList.add('choice-selected', 'tiedSelected');
+    } else {
+        choices.forEach(choice => {
+            if (choice.getAttribute('data-id') === computerChoice) {
+                choice.classList.add('computerSelected', 'choice-selected');
+            }
+        });
+        
+        selectedChoice.classList.add('choice-selected', 'playerSelected');
+    }
+
     showGame()
-    setTimeout(() => {
-        clearHTML()
-        clearChoice()
-    }, 3000);
 }
 
 function getComputerChoice() {
@@ -43,37 +67,49 @@ function getComputerChoice() {
 
 function showGame() {
     clearHTML()
-    const winner = document.createElement('p')
+    const winner = getWinner(playerChoice, computerChoice)
+    const winnerHTML = document.createElement('p')
     const gameDiv = document.createElement('div')
-
-    winner.textContent = getWinner(playerChoice, computerChoice)
-    winner.classList.add('winner-text')
-    gameDiv.classList.add('game-result')
-    if (result === 'winner') {
-        gameDiv.classList.add('winner')
-    } else if (result === 'loser') {
-        gameDiv.classList.add('loser')
-    } else {
-        gameDiv.classList.add('tied')
+    if (playerScore + computerScore >= 5) {
+        winnerHTML.textContent = playerScore > computerScore ? "You are the winner in the best of 5 rounds!" : "You lose, the CPU destroyed you!"
+        playerScore > computerScore ? gameDiv.classList.add('winner') : gameDiv.classList.add('loser')
     }
-    gameDiv.appendChild(winner)
-    game.appendChild(gameDiv)
+    else {
+        winnerHTML.textContent = winner
+        if (result === 'winner') {
+            gameDiv.classList.add('winner')
+        } else if (result === 'loser') {
+            gameDiv.classList.add('loser')
+        } else {
+            gameDiv.classList.add('tied')
+        }
+    }
+    winnerHTML.classList.add('winner-text')
+    gameDiv.classList.add('game-result')
+
+    scorePlayerHTML.textContent = playerScore;
+    scoreComputerHTML.textContent = computerScore;
+
+
+    gameDiv.appendChild(winnerHTML)
+    gameResult.appendChild(gameDiv)
 
 }
 
 function clearHTML() {
-    while (game.firstChild) {
-        game.removeChild(game.firstChild)
+    while (gameResult.firstChild) {
+        gameResult.removeChild(gameResult.firstChild)
     }
 }
 
 function clearChoice() {
     choices.forEach(choice => {
-        choice.classList.remove('choice-selected', 'computer');
+        choice.classList.remove('choice-selected', 'computer', 'playerSelected', 'computerSelected', 'tiedSelected');
     })
 }
 
 function getWinner(player, computer) {
+
     if (player === computer) {
         result = 'tied'
         return "Tied!"
@@ -82,10 +118,12 @@ function getWinner(player, computer) {
         || player === "paper" && computer === "scissors"
         || player === "scissors" && computer === "rock") { //second column
         result = 'loser'
+        computerScore++
         return `You Lose! ${computer} beats ${player}`
     }
     else { // third column
         result = 'winner'
+        playerScore++
         return `You Win! ${player} beats ${computer}`
     }
 }
