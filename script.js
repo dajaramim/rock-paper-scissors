42ec2443929aa41d302eccb28cfa96c38fa8b05d
Daniel Jaramillo Moya <da.jaramim@gmail.com>
2023-12-01 01:10:34 -0300

    feat: ui choice added and logic changed

--- script.js
+++ script.js
@@ -1,46 +1,63 @@
 const gameOption = ["rock", "paper", "scissors"]
-const choices = document.querySelector('#choices');
+const choices = document.querySelectorAll('#choice');
 const game = document.querySelector('#game');
+let result;
 let playerChoice;
 let computerChoice;
 
 eventListeners()
 function eventListeners() {
-    choices.addEventListener('click', getPlayerChoice)
+    choices.forEach(choice => {
+        choice.addEventListener('click', getPlayerChoice)
+    });
 }
 
 // Functions
 
 function getPlayerChoice(e) {
-    if (e.target.classList.value) {
-        playerChoice = e.target.classList.value
-        computerChoice = getComputerChoice()
-        showGame()
+    clearChoice()
+    computerChoice = getComputerChoice()
+    choices.forEach(choice => {
+        if (choice.getAttribute('data-id') === computerChoice) {
+            choice.classList.add('computer','choice-selected');
+        }
+    });
+    if (!e.target.classList.contains("choice")) {
+        playerChoice = e.target.parentNode.getAttribute('data-id')
+    } else {
+        playerChoice = e.target.getAttribute('data-id')
     }
+    const selectedChoice = !e.target.classList.contains("choice") ? e.target.parentNode : e.target;
+    selectedChoice.classList.add('choice-selected');
+    showGame()
+    setTimeout(() => {
+        clearHTML()
+        clearChoice()
+    }, 3000);
 }
 
 function getComputerChoice() {
-    // random number
-    let randomNumber = Math.floor(Math.random() * 3) // 3 because it is the number of available options in the game.
+    let randomNumber = Math.floor(Math.random() * gameOption.length) // 3 because it is the number of available options in the game.
     return gameOption[randomNumber]
 }
 
 function showGame() {
     clearHTML()
-    const playerGame = document.createElement('p')
-    const computerGame = document.createElement('p')
     const winner = document.createElement('p')
     const gameDiv = document.createElement('div')
 
-    playerGame.textContent = playerChoice;
-    computerGame.textContent = computerChoice;
     winner.textContent = getWinner(playerChoice, computerChoice)
-
-    gameDiv.appendChild(playerGame)
-    gameDiv.appendChild(computerGame)
+    winner.classList.add('winner-text')
+    gameDiv.classList.add('game-result')
+    if (result === 'winner') {
+        gameDiv.classList.add('winner')
+    } else if (result === 'loser') {
+        gameDiv.classList.add('loser')
+    } else {
+        gameDiv.classList.add('tied')
+    }
     gameDiv.appendChild(winner)
     game.appendChild(gameDiv)
-
 
 }
 
@@ -50,17 +67,26 @@ function clearHTML() {
     }
 }
 
+function clearChoice() {
+    choices.forEach(choice => {
+        choice.classList.remove('choice-selected', 'computer');
+    })
+}
+
 function getWinner(player, computer) {
     if (player === computer) {
+        result = 'tied'
         return "Tied!"
     }
     else if (player === "rock" && computer === "paper"
         || player === "paper" && computer === "scissors"
         || player === "scissors" && computer === "rock") { //second column
-        return `You lose! ${computer} beats ${player}`
+        result = 'loser'
+        return `You Lose! ${computer} beats ${player}`
     }
     else { // third column
-        return `You win! ${player} beats ${computer}`
+        result = 'winner'
+        return `You Win! ${player} beats ${computer}`
     }
 }
 /* const winner = playerScore > computerScore ? "You are the winner in the best of 5 rounds!" : "You lose, the computer has completely destroyed you!" */
